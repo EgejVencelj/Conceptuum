bac35319c419629bb339d6f176235d772d5754d5
Egej Vencelj <e.igi@hotmail.com>
2017-01-13 15:25:41 +0100

    Added mouse toggle switch support

--- Conceptuum/Assets/Scripts/Player.cs
+++ Conceptuum/Assets/Scripts/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+			if(Physics.Raycast(ray, out hit)) {
+				Transform objectHit = hit.transform;
+
+				Debug.Log(objectHit.name);
+				Switch s = objectHit.GetComponent<Switch>();
+				if(s != null) {
+					s.Toggle();
+				}
+				// Do something with the object that was hit by the raycast.
+			}
+		}
 	}
 }

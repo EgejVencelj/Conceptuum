3ac45226281c87bab50bc880ffc9ed5b0fe94412
Egej Vencelj <e.igi@hotmail.com>
2017-01-13 14:08:47 +0100

    Added wire generation with intermediate nodes.

--- Conceptuum/Assets/Logical Elements/Wire/Wire.cs
+++ Conceptuum/Assets/Logical Elements/Wire/Wire.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class Wire : BoolOutputElement {
+	public List<WireNode> nodes;
 	public BoolOutputElement inputBool;
 	Color c;
 
 	void Start() {
-		c = GetComponent<MeshRenderer>().materials[0].color;
 		c = Color.blue;
+
 		if(inputBool) {
 			inputBool.onStateChanged += UpdateState;
 		}
@@ -16,11 +21,55 @@ public class Wire : BoolOutputElement {
 
 
 	void UpdateState() {
-		outputBool = inputBool;
-		if(inputBool) {
+		outputBool = inputBool.outputBool;
+		if(inputBool.outputBool) {
 			c = Color.green;
-		}else {
+		} else {
 			c = Color.red;
+		}
+		foreach(WireNode n in nodes) {
+			if(n.wire != null) {
+				n.wire.GetComponent<MeshRenderer>().materials[0].color = c;
+			}
 		}
 	}
 }
+#if UNITY_EDITOR
+[CustomEditor(typeof(Wire))]
+public class WireEditor : Editor {
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		Wire el = (Wire)target;
+
+		if(GUILayout.Button("Add Node")) {
+
+			List<WireNode> nodes = el.nodes;
+			GameObject node = new GameObject("Node " + nodes.Count, typeof(WireNode));
+			node.transform.parent = el.transform;
+			node.transform.localPosition = Vector3.zero;
+
+			WireNode wn = node.GetComponent<WireNode>();
+			if(el.nodes.Count > 0) {
+				wn.from = el.nodes[el.nodes.Count - 1];
+			}
+
+			wn.parent = el;
+			el.nodes.Add(wn);
+		}
+
+			if(GUILayout.Button("Remove Node")) {
+				if(el.nodes.Count > 0) {
+				WireNode wn = el.nodes[el.nodes.Count - 1];
+				el.nodes.Remove(wn);
+				DestroyImmediate(wn.gameObject);
+			}
+		}
+
+		if(GUILayout.Button("Update Wires")) {
+			foreach(WireNode n in el.nodes) {
+				n.UpdateWires();
+			}
+		}
+	}
+}
+#endif
---
+++ Conceptuum/Assets/Logical Elements/Wire/WireNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireNode : MonoBehaviour {
+	public Wire parent;
+	public WireNode from;
+	public GameObject wire;
+
+	public void UpdateWires() {
+		if(wire != null) {
+			DestroyImmediate(wire);
+		}
+		if(from != null) {
+
+			wire = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+			wire.transform.parent = parent.transform;
+			wire.transform.position = (transform.position + from.transform.position) / 2.0f;
+			wire.transform.rotation = Quaternion.FromToRotation(Vector3.up, transform.position - from.transform.position);
+
+			var v3T = wire.transform.localScale;      // Scale it
+			v3T.y = (transform.position - from.transform.position).magnitude / 2;
+			v3T.x = 0.05f;
+			v3T.z = 0.05f;
+			wire.transform.localScale = v3T;
+		}
+	}
+}
